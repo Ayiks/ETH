cc40c670fd9ff55d48aab6d1ad1a1dbfd4aea081
kofikoi <appiahkorantemaah@gmail.com>
2023-05-16 16:01:25 +0000

    initial commit(WIP)

---
+++ wpdemo.archiwp.com/engitech/wp-admin/admin-ajax6312.php
@@ -0,0 +1,15 @@
+<HTML>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from wpdemo.archiwp.com/engitech/wp-admin/admin-ajax.php?action=process_simple_like&post_id=19&nonce=056d628a9c&is_comment=0&disabled=true by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 May 2023 11:42:03 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html;charset=UTF-8"><META HTTP-EQUIV="Refresh" CONTENT="0; URL=../how-to-make-your-ios-13-compatible/index.html"><TITLE>Page has moved</TITLE>
+</HEAD>
+<BODY>
+<A HREF="../how-to-make-your-ios-13-compatible/index.html"><h3>Click here...</h3></A>
+</BODY>
+<!-- Created by HTTrack Website Copier/3.49-2 [XR&CO'2014] -->
+
+<!-- Mirrored from wpdemo.archiwp.com/engitech/wp-admin/admin-ajax.php?action=process_simple_like&post_id=19&nonce=056d628a9c&is_comment=0&disabled=true by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 16 May 2023 11:42:03 GMT -->
+</HTML>
